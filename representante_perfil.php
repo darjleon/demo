2e201a127a3f592be85eca8dc9f335fdd36620b1
rockerlp <jfpal93@hotmail.com>
2017-04-12 13:14:22 -0500

    Se copia el contenido de padre_perfil.php a representante_perfil.php

--- representante_perfil.php
+++ representante_perfil.php
@@ -23,7 +23,6 @@
 
     <link href="css/animate.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
-
 </head>
 <body>
 <div id="wrapper">
@@ -35,40 +34,43 @@
                         <img alt="image" class="img-circle" src="img/a5.jpg" width="40%" />
                         <a href="#">
                             <span class="block ">
-                                <h3><strong class="font-bold">CINTHIA HERA RODRIGUEZ</strong></h3>
+                                <h4>
+                                    <strong class="font-bold">CINTHIA HERA RODRIGUEZ</strong>
+                                    <br><small class="profile-type">REPRESENTANTE</small>
+                                </h4>                                
                             </span>
                         </a>
                     </div>
                     <div class="logo-element">
-                        <img alt="logo" src="img/logo.png" width="50px" />
+                        <img alt="logo" src="img/logo unico.png" width="50px" />
                     </div>
                 </li>                
                 <li class="active">
-                    <a href="#">
-                        <i class="fa fa-th-large"></i> <span class="nav-label">Mi Perfil</span>
+                    <a href="padre.php">
+                        <i class="fa fa-th-large"></i><span class="nav-label">Mi Perfil</span>
                     </a>
                 </li>
                 <li>
-                    <a href="padre_institucion.html">
-                        <i class="fa fa-institution"></i> <span class="nav-label">Institucion</span> 
+                    <a href="padre_institucion.php">
+                        <i class="fa fa-institution"></i><span class="nav-label">Institucion</span> 
                     </a>
                 </li>
                 <li>
-                    <a href="padre_notificacion.html">
+                    <a href="padre_notificacion.php">
                         <i class="fa fa-newspaper-o"></i>
                         <span class="nav-label">Notificaciones </span>
                         <span class="label label-warning">Nuevo</span>
                     </a>
                 </li>
                 <li>
-                    <a href="padre_alu.html">
+                    <a href="#">
                         <img src="img/icono persona white.png" width="15px">
                         <span class="nav-label" style="padding-left:8px">Alumno</span>
                         <span class="fa arrow"></span>
                     </a>
                     <ul class="nav nav-second-level">
-                        <li><a href="padre_hijo1.html"><img src="img/icono persona.png" width="20px"> ELKIN DAVID SANNA HERA</a></li>
-                        <li><a href="padre_hijo2.html"><img src="img/icono persona.png" width="20px"> ALAN JOEL SANNA HERA</a></li>
+                        <li><a href="padre_hijo1.php"><img src="img/icono persona.png" width="20px"> ELKIN DAVID SANNA HERA</a></li>
+                        <li><a href="padre_hijo2.php"><img src="img/icono persona.png" width="20px"> ALAN JOEL SANNA HERA</a></li>
                     </ul>
                 </li>                                     
             </ul>
@@ -91,29 +93,11 @@
                     <li class="dropdown">
                         <a class="dropdown-toggle count-info" data-toggle="dropdown" href="#">
                             <i class="fa fa-bell"></i><span class="label label-primary">1</span>
-                        </a>
-                        <ul class="dropdown-menu dropdown-alerts">
-                            <li>
-                                <a href="p_notificacion.html">
-                                    <div>
-                                        <i class="fa fa-upload fa-fw"></i> Coordinación Académica
-                                        <span class="pull-right text-muted small">ayer(11.15 am)</span>
-                                    </div>
-                                </a>
-                            </li>
-                            <li class="divider"></li>
-                            <li>
-                                <div class="text-center link-block">
-                                    <a href="p_notificacion.html">
-                                        <strong>See All Alerts</strong><i class="fa fa-angle-right"></i>
-                                    </a>
-                                </div>
-                            </li>
-                        </ul>
+                        </a>                        
                     </li>
                     <li>
                         <a href="logout.php">
-                            <i class="fa fa-sign-out"></i>Cerrar Sesión
+                            <i class="fa fa-sign-out"></i> Cerrar Sesion
                         </a>
                     </li>
                 </ul>
@@ -145,7 +129,7 @@
                                     <a data-toggle="tab" href="#tab-3">PROFESION</a>
                                 </li>
                                 <li>
-                                    <a data-toggle="tab" href="#tab-4">MÉDICA</a>
+                                    <a data-toggle="tab" href="#tab-4">MEDICA</a>
                                 </li>
                             </ul>
                             <div class="tab-content">
@@ -368,6 +352,8 @@
                 toastr.success('Representante', 'Bienvenido a Pined');
 
             }, 1300);
+
+
             
             var doughnutData = [
                 {
