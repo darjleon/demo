31524f76eb95c9219a706fb7eb0b434cccb2b733
rockerlp <jfpal93@hotmail.com>
2017-05-10 13:12:07 -0500

    Llenado de vista de asistencia de 10A en perfil director

--- director/include/10Aasistencia.php
+++ director/include/10Aasistencia.php
@@ -12,8 +12,8 @@
                                     <th class="text-center" style="background-color: #939598;color: white;width:10%">Viernes</th>
                                 </tr>
                             </thead>
-                            <tbody style="background-color:white">                            
-                                <tr>
+                            <tbody style="background-color:white" id="asistencia-list">                            
+                                <!--<tr>
                                     <td>
                                         <p class="student-list">
                                             <span class="list-asist">1</span>
@@ -652,9 +652,224 @@
                                             </div>
                                         </td>
                                     </form>
-                                </tr>
+                                </tr>-->
                             </tbody>
                         </table>
                     </div>            
                 </div>
             </div>
+<!--<tr>
+                                    <td>
+                                        <p class="student-list">
+                                            <span class="list-asist">1</span>
+                                            ALEXANDRA PAOLA FIGUEROA LA SERNA
+                                        </p>
+                                    </td>
+                                    <form>
+                                        <td class="text-center">
+                                            <div class="checkboxNotas"> 
+                                                <input class="text-center" type="checkbox" value="d1" id="checkboxNotas1">
+                                                <label for="checkboxNotas1"></label>
+                                            </div>
+                                        </td>
+                                        <td class="text-center">
+                                            <div class="checkboxNotas"> 
+                                                <input class="text-center" type="checkbox" value="d2" id="checkboxNotas2">
+                                                <label for="checkboxNotas2"></label>
+                                            </div>
+                                        </td>
+                                        <td class="text-center">
+                                            <div class="checkboxNotas"> 
+                                                <input class="text-center" type="checkbox" value="d3" id="checkboxNotas3">
+                                                <label for="checkboxNotas3"></label>
+                                            </div>
+                                        </td>
+                                        <td class="text-center">
+                                            <div class="checkboxNotas"> 
+                                                <input class="text-center" type="checkbox" value="d3" id="checkboxNotas3">
+                                                <label for="checkboxNotas3"></label>
+                                            </div>
+                                        </td>
+                                        <td class="text-center">
+                                            <div class="checkboxNotas"> 
+                                                <input class="text-center" type="checkbox" value="d3" id="checkboxNotas3">
+                                                <label for="checkboxNotas3"></label>
+                                            </div>
+                                        </td>
+                                    </form>
+                                </tr>-->
+
+<script type="text/javascript">
+
+     var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          getAsistencia(this);
+          
+        }
+      };
+      xhttp.open("GET", "xml/course10A.xml", true);
+      xhttp.send();
+
+      function getAsistencia(xml){
+        var i,j,k;
+          var xmlDoc = xml.responseXML;
+         //var table="<p><strong>Profesores</strong></p>";
+          var x = xmlDoc.getElementsByTagName("student");
+          for (i = 0; i <x.length; i++) { 
+            var newtr = document.createElement('tr');
+            var td1 = document.createElement('td');
+            var pStd = document.createElement('p');
+            pStd.setAttribute('class','student-list');
+
+            /*var spanStd = document.createElement('span');
+            spanStd.setAttribute('class','list-asist');*/
+            pStd.innerHTML='<span class="list-asist">'+(i+1)+'</span>'+"  "+x[i].getElementsByTagName("name")[0].childNodes[0].nodeValue + " " + x[i].getElementsByTagName("lastName")[0].childNodes[0].nodeValue;
+
+            
+            td1.appendChild(pStd);
+            newtr.appendChild(td1);
+
+
+
+            var form = document.createElement('form');
+            var td2 = document.createElement('td');
+            td2.setAttribute('class','text-center');
+
+            var divCheck = document.createElement('div');
+            divCheck.setAttribute('class',"checkboxNotas");
+
+            var input = document.createElement('input');
+            input.setAttribute('class',"text-center");
+            input.setAttribute('type',"checkbox");
+            input.setAttribute('value',"d1");
+            input.setAttribute('id',"checkboxNotas1");
+
+            var label = document.createElement('label');
+            label.setAttribute('for',"checkboxNotas1");
+
+            divCheck.appendChild(input);
+            divCheck.appendChild(label);
+
+            td2.appendChild(divCheck);
+
+            form.appendChild(td2);
+
+
+
+            var td3 = document.createElement('td');
+            td3.setAttribute('class','text-center');
+
+            var divCheck2 = document.createElement('div');
+            divCheck2.setAttribute('class',"checkboxNotas");
+
+            var input2 = document.createElement('input');
+            input2.setAttribute('class',"text-center");
+            input2.setAttribute('type',"checkbox");
+            input2.setAttribute('value',"d2");
+            input2.setAttribute('id',"checkboxNotas2");
+
+            var label2 = document.createElement('label');
+            label2.setAttribute('for',"checkboxNotas2");
+
+            divCheck2.appendChild(input2);
+            divCheck2.appendChild(label2);
+
+            td3.appendChild(divCheck2);
+
+            form.appendChild(td3);
+
+
+
+            var td4 = document.createElement('td');
+            td4.setAttribute('class','text-center');
+
+            var divCheck3 = document.createElement('div');
+            divCheck3.setAttribute('class',"checkboxNotas");
+
+            var input3 = document.createElement('input');
+            input3.setAttribute('class',"text-center");
+            input3.setAttribute('type',"checkbox");
+            input3.setAttribute('value',"d3");
+            input3.setAttribute('id',"checkboxNotas3");
+
+            var label3 = document.createElement('label');
+            label3.setAttribute('for',"checkboxNotas3");
+
+            divCheck3.appendChild(input3);
+            divCheck3.appendChild(label3);
+
+            td4.appendChild(divCheck3);
+
+            form.appendChild(td4);
+
+
+
+            var td5 = document.createElement('td');
+            td5.setAttribute('class','text-center');
+
+            var divCheck4 = document.createElement('div');
+            divCheck4.setAttribute('class',"checkboxNotas");
+
+            var input4 = document.createElement('input');
+            input4.setAttribute('class',"text-center");
+            input4.setAttribute('type',"checkbox");
+            input4.setAttribute('value',"d3");
+            input4.setAttribute('id',"checkboxNotas3");
+
+            var label4 = document.createElement('label');
+            label4.setAttribute('for',"checkboxNotas3");
+
+            divCheck4.appendChild(input4);
+            divCheck4.appendChild(label4);
+
+            td5.appendChild(divCheck4);
+
+            form.appendChild(td5);
+
+
+
+            var td6 = document.createElement('td');
+            td6.setAttribute('class','text-center');
+
+            var divCheck5 = document.createElement('div');
+            divCheck5.setAttribute('class',"checkboxNotas");
+
+            var input5 = document.createElement('input');
+            input5.setAttribute('class',"text-center");
+            input5.setAttribute('type',"checkbox");
+            input5.setAttribute('value',"d3");
+            input5.setAttribute('id',"checkboxNotas3");
+
+            var label5 = document.createElement('label');
+            label5.setAttribute('for',"checkboxNotas3");
+
+            divCheck5.appendChild(input5);
+            divCheck5.appendChild(label5);
+
+            td6.appendChild(divCheck5);
+
+            form.appendChild(td6);
+
+
+
+            newtr.appendChild(form);
+
+
+
+
+
+
+
+
+
+
+            document.getElementById("asistencia-list").appendChild(newtr);
+
+
+          }
+
+
+
+      }
+</script>
